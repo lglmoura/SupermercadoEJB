6b675da1ba5146147bb3a10b4e43389199fa5963
Luiz Gustavo L Moura <gustavo@iff.edu.br>
2014-08-12 20:58:49 -0300

    Criando o Classe de teste

---
+++ ejbModule/br/iff/pooa20141/supermercado/model/test/FabricanteTest.java
@@ -0,0 +1,50 @@
+package br.iff.pooa20141.supermercado.model.test;
+
+import static org.junit.Assert.*;
+
+import java.util.UUID;
+
+import javax.naming.Context;
+import javax.naming.NamingException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.iff.pooa20141.supermercado.model.controller.FabricantePersistenceRemote;
+import br.iff.pooa20141.supermercado.model.entity.Fabricante;
+
+public class FabricanteTest {
+	
+	private FabricantePersistenceRemote fabricantePR;
+
+	@Before
+	public void setUp() throws Exception {
+		Context context = null;
+		fabricantePR = null;
+		try {
+
+			context = JNDILookupClass.getInitialContext();
+
+			fabricantePR = (FabricantePersistenceRemote) context
+					.lookup("ejb:SupermercadoEAR/SupermercadoEJB/FabricantePersistence!br.iff.pooa20141.supermercado.model.controller.FabricantePersistenceRemote");
+
+		} catch (NamingException e) {
+			e.printStackTrace();
+
+		}
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testInserir() {
+		String uid = UUID.randomUUID().toString();
+		Fabricante fabricante = fabricantePR.inserir(uid,"Nome");
+		assertEquals(fabricante.getUid(), uid);
+		fabricantePR.delete(uid);
+	}
+
+}
