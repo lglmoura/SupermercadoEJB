f5673c866800c7746382b3c866d178aeea16b2db
Luiz Gustavo L Moura <gustavo@iff.edu.br>
2014-08-12 19:28:59 -0300

    Criando o Interface Remota 2

--- ejbModule/br/iff/pooa20141/supermercado/model/controller/FabricantePersistenceRemote.java
+++ ejbModule/br/iff/pooa20141/supermercado/model/controller/FabricantePersistenceRemote.java
@@ -1,5 +1,25 @@
 package br.iff.pooa20141.supermercado.model.controller;
 
+import java.util.List;
+
+import javax.ejb.Remote;
+import javax.xml.bind.annotation.XmlElement;
+
+import br.iff.pooa20141.supermercado.model.entity.Fabricante;
+
+
+@Remote
 public interface FabricantePersistenceRemote {
+
+	public Fabricante inserir(String uid,  String nome);
+
+	public Fabricante update(String uid, String nome);
+
+	public Fabricante find(String uid);
+
+	public void delete(String uid);
+
+	@XmlElement(name = "fabricante")
+	public List<Fabricante> findAll();
 
 }
